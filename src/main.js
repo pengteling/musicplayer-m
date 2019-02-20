993127b42a508b8b6a456253a48c87bfa04f0357
pengteling <pengteling@gmail.com>
2019-02-20 20:48:58 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App'
+
+/* eslint no-new:"off" */
+new Vue({
+  el: '#app',
+  // components:{
+  //   App
+  // },
+  // template:'<App/>'
+  render: h => h(App),
+})
